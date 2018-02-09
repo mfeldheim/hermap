865fb18b57747a6dd292bd3f24470d306770ea62
Michel Feldheim <m.feldheim@opendi.com>
2018-02-09 11:48:41 +0100

    fixed city names in example

--- examples/drawPointsOnMap.php
+++ examples/drawPointsOnMap.php
@@ -25,19 +25,19 @@ $mapFile = './resources/Normal_Mercator_map_85deg.jpg';
 
 $coords = array
 (
-    'Santiago, CL'      => array( -33.469120, -70.641997 ),
-    'New York, US'      => array( 40.714353, -74.005973 ),
-    'Ottawa, CA'        => array( 45.421530, -75.697193 ),
-    'Bejing, ZH'        => array( 39.904030, 116.407526 ),
-    'Wellington, NZ'    => array( -41.286460, 174.776236),
-    'Berlin, DE'        => array( 52.519171, 13.406091 ),
-    'Anchorage, AK'     => array( 61.218056, -149.900278 ),
-    'Bergen, NO'        => array( 60.391263, 5.322054 ),
-    'Reykjavik, IS'     => array( 64.135338, -21.895210 ),
-    'Nordvik, RU'       => array( 74.018100, 111.469000),
-    'Rio d Janeiro, BR' => array( -22.903539, -43.209587 ),
-    'Cape Town, ZA'     => array( -33.924869, 18.424055 ),
-    'Brrrr, cold'       => array( -82.494824, -52.910156 )
+    'Santiago, CL'       => array( -33.469120, -70.641997 ),
+    'New York, US'       => array( 40.714353, -74.005973 ),
+    'Ottawa, CA'         => array( 45.421530, -75.697193 ),
+    'Beijing, ZH'        => array( 39.904030, 116.407526 ),
+    'Wellington, NZ'     => array( -41.286460, 174.776236),
+    'Berlin, DE'         => array( 52.519171, 13.406091 ),
+    'Anchorage, AK'      => array( 61.218056, -149.900278 ),
+    'Bergen, NO'         => array( 60.391263, 5.322054 ),
+    'Reykjavik, IS'      => array( 64.135338, -21.895210 ),
+    'Nordvik, RU'        => array( 74.018100, 111.469000),
+    'Rio de Janeiro, BR' => array( -22.903539, -43.209587 ),
+    'Cape Town, ZA'      => array( -33.924869, 18.424055 ),
+    'Brrrr, cold'        => array( -82.494824, -52.910156 )
 
 );
 
@@ -45,7 +45,7 @@ $image = imagecreatefromjpeg( $mapFile );
 $red = imagecolorallocate( $image, 255, 0, 0 );
 $white = imagecolorallocate( $image, 255, 255, 255 );
 
-foreach( $coords as $city => $coord ) {
+foreach ($coords as $city => $coord) {
     $res = $mapProjection->degreesToPixels( $coord[0], $coord[1], $mapWidth, $mapHeight );
     imagefilledellipse( $image, $res['x'], $res['y'], 10, 10, $red );
     if ( function_exists( 'imagefttext' ) ) {
@@ -53,7 +53,7 @@ foreach( $coords as $city => $coord ) {
     }
 }
 
-if ( PHP_SAPI !== 'cli' ) {
+if (PHP_SAPI !== 'cli') {
     header( 'Content-Type: image/png' );
     imagepng( $image );
 } else {
